94af47bc985357e521af49e3ebf19d52fce4de26
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-06-07 19:16:08 -0400

    something wasnt commited

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const router = require("./router");
 
 const server = express();
 
 server.use(helmet());
+server.use(cors());
 server.use(express.json());
 server.use("/router", router);
 
